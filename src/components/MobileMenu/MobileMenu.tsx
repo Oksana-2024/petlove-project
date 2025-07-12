b7b9e35e5ee23151cfc7fef5a2e2dc45c8398040
Oksana <oksanavasilevna77@gmail.com>
2025-07-12 21:18:49 +0300

    add component  MobileMenu

---
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,78 @@
+import Navigation from "../Navigation/Navigation";
+import Icon from "../Icon/Icon";
+import { useEffect, useRef } from "react";
+
+import { Link } from "react-router-dom";
+import { useAuth } from "../../hook/useAuth";
+import BaseButton from "../BaseButton/BaseButton";
+import s from "./MobileMenu.module.css";
+
+interface IMobileMenu {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const MobileMenu = ({ isOpen, onClose }: IMobileMenu) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <div ref={modalRef} className={`${s.modal} ${isOpen && s.modalOpen}`}>
+      <button className={s.closeBtn} onClick={onClose} aria-label="Close">
+        <Icon name="icon-close" size={16} className={s.closeIcon} />
+      </button>
+
+      <nav>
+        <Navigation onClick={onClose} className={s.listMenu} variant={false} />
+      </nav>
+      <div className={s.linkWrapper} onClick={onClose}>
+        {isLoggedIn ? (
+          <BaseButton
+            text="Log out"
+            type="button"
+            style={s.logoutBtn}
+            onClick={() => console.log("logout mobile")}
+          />
+        ) : (
+          <>
+            <Link to="/login" className={s.loginLink}>
+              Log In
+            </Link>
+            <Link to="/register" className={s.registerLink}>
+              Registration
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
