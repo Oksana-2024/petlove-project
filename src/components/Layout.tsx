9c35f46cb27a7c59176f02d071714a053b6c8ec2
Oksana <oksanavasilevna77@gmail.com>
2025-07-12 21:21:53 +0300

    setting routes

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+import Header from "./Header/Header";
+import { Outlet, useLocation } from "react-router-dom";
+import { ThemeProvider } from "../context/ThemeContext";
+
+interface ILayoute {
+  children?: ReactNode;
+}
+
+const Layout = ({ children }: ILayoute) => {
+  const location = useLocation();
+  const isHome = location.pathname === "/home";
+  return (
+    <ThemeProvider theme={isHome ? "home" : "default"}>
+      <Header />
+      <main>{children}</main>
+      <Outlet />
+    </ThemeProvider>
+  );
+};
+
+export default Layout;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,37 @@
+import { lazy } from "react";
+import { type RouteObject } from "react-router-dom";
+import Layout from "../components/Layout";
+
+const Main = lazy(() => import("../pages/MainPage/MainPage"));
+const Home = lazy(() => import("../pages/HomePage/HomePage"));
+const Register = lazy(() => import("../pages/RegisterPage/RegisterPage"));
+const Login = lazy(() => import("../pages/LoginPage/LoginPage"));
+const News = lazy(() => import("../pages/NewsPage/NewsPage"));
+const Notices = lazy(() => import("../pages/NoticesPage/NoticesPage"));
+const Friends = lazy(() => import("../pages/OurFriendsPage/OurFriendsPage"));
+const Profile = lazy(() => import("../pages/ProfilePage/ProfilePage"));
+const AddPet = lazy(() => import("../pages/AddPet/AddPet"));
+const NotFound = lazy(() => import("../pages/NotFoundPage/NotFounPage"));
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    element: <Layout />, 
+    children: [
+      { path: "/home", element: <Home /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      { path: "/news", element: <News /> },
+      { path: "/notices", element: <Notices /> },
+      { path: "/friends", element: <Friends /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/add-pet", element: <AddPet /> },
+      { path: "*", element: <NotFound /> }, 
+    ],
+  },
+];
+
+export default routes;
