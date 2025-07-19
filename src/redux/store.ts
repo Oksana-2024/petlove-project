e981b0a6e4da83017ba5e986f0e304f348ffe965
Oksana <oksanavasilevna77@gmail.com>
2025-07-19 15:03:15 +0300

    update store: add reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/slice";
+import newsReducer from "./news/slice";
+import friendsReducer from "./friends/slice";
+import noticesReducer from "./notices/slice";
 
 const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: {
+    user: userReducer,
+    news: newsReducer,
+    friends: friendsReducer,
+    notices: noticesReducer,
+  },
 });
 
 export type StoreType = ReturnType<typeof store.getState>;
