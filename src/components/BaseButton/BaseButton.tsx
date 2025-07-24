82244db0d00f08bdb0fe2d7bd4ca08514fe35f06
Oksana <oksanavasilevna77@gmail.com>
2025-07-24 12:07:58 +0300

    style(login-form): add hover effects for BaseButton and Link

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -1,17 +1,29 @@
 import clsx from "clsx";
 import s from "./BaseButton.module.css";
+import Icon from "../Icon/Icon";
+
 type ButtonType = "button" | "submit" | "reset";
 interface IBaseButton {
   text: string;
   type?: ButtonType;
   style?: string;
   onClick?: () => void;
+  icon?: string;
+  iconStyle?: string;
 }
 
-const BaseButton = ({ text, type = "submit", style, onClick }: IBaseButton) => {
+const BaseButton = ({
+  text,
+  type = "submit",
+  style,
+  onClick,
+  icon,
+  iconStyle,
+}: IBaseButton) => {
   return (
     <button className={clsx(s.baseStyle, style)} type={type} onClick={onClick}>
       {text}
+      {icon && <Icon name={icon} className={iconStyle} />}
     </button>
   );
 };
