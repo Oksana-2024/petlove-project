b3e885302a6b6d966a35f6be701d7e8c32e2da3d
Oksana <oksanavasilevna77@gmail.com>
2025-07-30 20:52:05 +0300

    feat: add PetBlock

--- src/components/PetBlock/PetBlock.tsx
+++ src/components/PetBlock/PetBlock.tsx
@@ -1,9 +1,36 @@
-//import s from './PetBlock'
+import useMedia from "../../hook/useMedia";
+import s from "./PetBlock.module.css";
 
 const PetBlock = () => {
+  const { isBigScreen, isMobile, isTablet } = useMedia();
   return (
-    <div>PetBlock</div>
-  )
-}
+    <div>
+      {isMobile && (
+        <img
+          src="/add_pet.webp"
+          alt="Dog"
+          srcSet="/add_pet@2x.webp 2x"
+          className={s.dogImage}
+        />
+      )}
+      {isTablet && (
+        <img
+          src="/add_pet_t.webp"
+          alt="Dog"
+          srcSet="/add_pet_t@2x.webp 2x"
+          className={s.dogImage}
+        />
+      )}
+      {isBigScreen && (
+        <img
+          src="/add_pet_d.webp"
+          alt="Dog"
+          srcSet="/add_pet_d@2x.webp 2x"
+          className={s.dogImage}
+        />
+      )}
+    </div>
+  );
+};
 
-export default PetBlock
+export default PetBlock;
