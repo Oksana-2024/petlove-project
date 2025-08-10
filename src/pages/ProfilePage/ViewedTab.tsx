f9806af936a0a6f321b0f1d3ca97ec07867ebade
Oksana <oksanavasilevna77@gmail.com>
2025-08-10 21:16:45 +0300

    ui: display info text on empty page

--- src/pages/ProfilePage/ViewedTab.tsx
+++ src/pages/ProfilePage/ViewedTab.tsx
@@ -6,11 +6,24 @@ import s from "./ViewedTab.module.css";
 
 const ViewedTab = () => {
   const notices = useSelector(selectViewed);
+
+  if (notices.length < 1) {
+    return (
+      <p className={s.text}>
+        Oops,
+        <span className={s.textAccent}>
+          looks like there aren't any furries
+        </span>
+        on our adorable page yet. Do not worry! View your pets on the "find your
+        favorite pet" page and add them to your favorites.
+      </p>
+    );
+  }
   return (
     <ul className={s.viewedList}>
       {notices.map((item) => (
         <li key={item._id} className={s.viewedItem}>
-          <NoticesItem {...item} isViewed/>
+          <NoticesItem {...item} isViewed />
         </li>
       ))}
     </ul>
